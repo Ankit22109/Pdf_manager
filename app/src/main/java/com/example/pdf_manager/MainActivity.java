f5420d0c7e4585ea0e9a9f59ec4cc7ffdf045a9f
Abhimanyu Yadav <abhimanyuyadav352441@gmail.com>
2024-03-20 12:32:19 +0530

    Adding Extra features

--- app/src/main/java/com/example/pdf_manager/MainActivity.java
+++ app/src/main/java/com/example/pdf_manager/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.pdf_manager;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
@@ -15,30 +24,32 @@ import android.content.Intent;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.pdf.PdfDocument;
+import android.graphics.pdf.PdfDocument.*;
 
 import com.example.pdf_manager.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileOutputStream;
 
 
 public class MainActivity extends AppCompatActivity {
-
-//    private static ActivityMainBinding onCreate;
+    private static final int PERMISSION_REQUEST_CODE = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Please install file manager", Toast.LENGTH_SHORT).show();
 
         FloatingActionButton create_btn = findViewById(R.id.create);
         FloatingActionButton edit_btn = findViewById(R.id.edit);
 
         edit_btn.setOnClickListener(v ->{
             FileSelector();
+
         });
 
         create_btn.setOnClickListener(v ->{
@@ -46,11 +57,49 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
 
+    private void makePermissonRequest(){
+        if(checkPermission()){
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }else{
+            requestPermission();
+        }
 
     }
 
+    private boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
 
+    private void requestPermission() {
+        // requesting permissions if not provided.
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+
+                // after requesting permissions we are showing
+                // users a toast message of permission granted.
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }
 
 
     private void FileSelector() {
@@ -72,25 +121,66 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == 101 && requestCode == Activity.RESULT_OK && data != null) {
 
             Uri uri = data.getData();
+//            String path = uri.getPath();
+//            File file = new File(path);
+
+            Intent send = new Intent(getApplicationContext(), OpenFile.class);
+            send.putExtra("Uri", uri.toString());
+
+            startActivityForResult(send, 102);
+
+
+            }
+        if (resultCode == 103 && requestCode == Activity.RESULT_OK && data != null) {
+            Uri uri = data.getData();
             String path = uri.getPath();
             File file = new File(path);
-            Intent send = new Intent(MainActivity.this, OpenFile.class);
-            send.putExtra("Uri", uri);
-            send.putExtra("file", file.toString());
-            send.putExtra("path", path);
-            startActivityForResult(send, 102);
+            Paint title = new Paint();
+            // create a new document
+            PdfDocument document = new PdfDocument();
+
+            // create a page description
+            PageInfo pageInfo = new PageInfo.Builder(100, 100, 1).create();
+
+            title.setTextSize(18);
+            title.setColor(ContextCompat.getColor(this, R.color.black));
+            title.setTextAlign(Paint.Align.CENTER);
+
+            // start a page
+            Page page = document.startPage(pageInfo);
+            Canvas canvas = page.getCanvas();
+
+            canvas.drawText("This is sample document which we have created.", 396, 560, title);
+            // draw something on the page
+//            View content = getContentView();
+//            content.draw(page.getCanvas());
+
+            // finish the page
+            document.finishPage(page);
+
+            // write the document content
+            try {
+                document.writeTo(new FileOutputStream(file));
+            } catch (IOException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+                throw new RuntimeException(e);
             }
+
+            // close the document
+            document.close();
+
         }
+
+    }
 
     public void CreateFile(){
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.setType("application/pdf");
-        PdfDocument Pdf_doc = new PdfDocument();
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(100,
-                100, 1).create();
-        PdfDocument.Page page = Pdf_doc.startPage(pageInfo);
 
         intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+
+
 
         try {
             startActivityForResult(intent, 103);
--- app/src/main/java/com/example/pdf_manager/OpenFile.java
+++ app/src/main/java/com/example/pdf_manager/OpenFile.java
@@ -1,8 +1,14 @@
 package com.example.pdf_manager;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.ParcelFileDescriptor;
+import android.widget.ImageView;
+
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +16,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class OpenFile extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+import java.io.IOException;
+
+public class OpenFile extends AppCompatActivity {
+    private static final int page_index = 1;
+
+    private void FileSelector() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(intent, 101);
+        } catch (Exception e) {
+            Toast.makeText(this, "Please install file manager", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +45,47 @@ public class OpenFile extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView txt = findViewById(R.id.txt);
-        Intent receive = new Intent( );
-        String file  = receive.getStringExtra("file");
-        txt.setText("File:" + file);
+
+        Intent intent = getIntent();
+
+        Uri uri = Uri.parse(intent.getStringExtra("Uri"));
+        String path = uri.getPath();
+        File file = new File(path);
+//        File destination = new File(file.getPath());
+        try {
+            Open(file);
+        } catch (IOException e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            throw new RuntimeException(e);
+        }
+
     }
+
+
+    private  void Open(File file) throws IOException {
+//        Toast.makeText(this, file.toString(), Toast.LENGTH_LONG).show();
+        ImageView ImgView = findViewById(R.id.ImgView);
+        try {
+            ParcelFileDescriptor FD = new ParcelFileDescriptor(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
+            PdfRenderer render = new PdfRenderer(FD);
+
+            PdfRenderer.Page page  = render.openPage(page_index);
+
+
+            Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_4444);
+            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            ImgView.setImageBitmap(bitmap);
+            page.close();
+            render.close();
+
+        }catch(FileNotFoundException e){
+            Toast.makeText(this, e + file.toString(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+//        page.close();
+//        render.close();
+    }
+
+
 }
